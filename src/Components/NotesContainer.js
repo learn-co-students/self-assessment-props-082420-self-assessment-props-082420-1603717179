1d1f0a2f7d40ac83dd293cb3b7a1284879d110d2
github username <github email address>
2020-08-02 18:16:41 -0700

    first edit

---
+++ src/Components/NotesContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NotesContainer() {
+
+    apiResponse = () => {
+        return ["First Note", "Second Note", "Third Note", "Fourth Note"]
+    }
+
+    return (
+        <ul>
+            {/* render notes here */}
+        </ul>
+    )
+}
