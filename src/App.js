60961ce91e676274464f318f0b311b33a60aac3d
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-26 15:03:55 -0400

    all deliverables done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from './Components/Header'
+import NotesContainer from './Components/NotesContainer'
 
 class App extends Component {
   render() {
-    return <header />
+    return <div><Header header="sample header"/>
+    <NotesContainer /></div>
   }
 }
 
